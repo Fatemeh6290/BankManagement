99118c658f3a5201091c399b43bf58e44e2b6e4c
Fatemeh Naseri <f.naseri@cp-pro.de>
2026-08-14 16:52:41 +0200

    UI implementiert

---
+++ BankManagement/Program.cs
@@ -0,0 +1,9 @@
+﻿using BankManagement.Service;
+using BankManagement.UI;
+
+CustomerService customerService = new CustomerService();
+TransactionService transactionService = new TransactionService();
+AccountService  accountService = new AccountService(customerService, transactionService);
+
+BankMenu bankMenu = new BankMenu(customerService, transactionService, accountService);
+bankMenu.ShowMenu();
---
+++ BankManagement/UI/BankMenu.cs
@@ -0,0 +1,529 @@
+using BankManagement.Enums;
+using BankManagement.Service;
+
+namespace BankManagement.UI;
+
+public class BankMenu
+{
+    private readonly CustomerService _customerService;
+    private readonly TransactionService _transactionService;
+    private readonly AccountService _accountService;
+    
+    public BankMenu(CustomerService customerService, TransactionService transactionService, AccountService accountService)
+    {
+        _customerService = customerService;
+        _transactionService = transactionService;
+        _accountService = accountService;
+    }
+    
+    public void ShowMenu()
+    {
+        while (true)
+        {
+            Console.WriteLine("===== Bank Management =====");
+            Console.WriteLine("1 - Customer Management");
+            Console.WriteLine("2 - Account Management");
+            Console.WriteLine("3 - Transaction");
+            Console.WriteLine("0 - Exit");
+
+            if (!int.TryParse(Console.ReadLine(), out int input))
+            {
+                Console.WriteLine("Please enter a valid option."); 
+                continue;
+            }
+
+            switch (input)
+            {
+                case 0:
+                    return;
+                case 1:
+                    CustomerManagement();
+                    break;
+                case 2:
+                    AccountManagement();
+                    break;
+                case 3:
+                    TransactionManagement();
+                    break;
+                default:
+                    Console.WriteLine("Please enter a valid option.");
+                    break;
+            }
+        }
+    }
+
+    private void CustomerManagement()
+    {
+        while (true)
+        {
+            Console.WriteLine("===== Customer Management =====");
+            Console.WriteLine("1 - Add Customer");
+            Console.WriteLine("2 - Show Customer");
+            Console.WriteLine("3 - Search Customer By Id");
+            Console.WriteLine("4 - Search Customer By Name");
+            Console.WriteLine("5 - Delete Customer");
+            Console.WriteLine("0 - Back");
+
+            if (!int.TryParse(Console.ReadLine(), out int input))
+            {
+                Console.WriteLine("Please enter a valid option.");
+                continue;
+            }
+
+            switch (input)
+            {
+                case 0:
+                    return;
+                case 1:
+                    AddCustomer();
+                    break;
+                case 2:
+                    ShowCustomer();
+                    break;
+                case 3:
+                    SearchCustomerById();
+                    break;
+                case 4:
+                    SearchCustomerByName();
+                    break;
+                case 5:
+                    DeleteCustomer();
+                    break;
+                default:
+                    Console.WriteLine("Please enter a valid option.");
+                    break;
+            }
+        }
+    }
+
+    private void AddCustomer()
+    {
+        Console.WriteLine("Enter your name:");
+        var name = Console.ReadLine();
+        
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            Console.WriteLine("Please enter a valid name.");
+            return;
+        }
+        
+        Console.WriteLine("Enter your email:");
+        var email = Console.ReadLine();
+        
+        if (string.IsNullOrWhiteSpace(email) || !email.Contains("@"))
+        {
+            Console.WriteLine("Please enter a valid email.");
+            return;
+        }
+        
+        _customerService.AddCustomer(name, email);
+        Console.WriteLine("Customer add successfully.");
+    }
+    
+    private void ShowCustomer()
+    {
+        Console.WriteLine("Customer list:");
+        var customers = _customerService.GetCustomers();
+
+        if (customers.Count == 0)
+        {
+            Console.WriteLine("No customer found.");
+            return;
+        }
+
+        foreach (var customer in customers)
+        {
+            Console.WriteLine($"Customer Id:{customer.CustomerId} - Name:{customer.Name} - Email:{customer.Email}");
+        }        
+    }
+    
+    private void SearchCustomerById()
+    {
+        Console.WriteLine("Enter your customer ID:");
+        int.TryParse(Console.ReadLine(), out int customerId);
+        
+        if (customerId <= 0)
+        {
+            Console.WriteLine("Please enter a valid customer ID.");
+            return;
+        }
+        
+        var customer = _customerService.GetCustomerById(customerId);
+
+        if (customer == null)
+        {
+            Console.WriteLine("No customer found.");
+            return;
+        }
+        
+        Console.WriteLine($"Customer Id:{customer.CustomerId} - Name:{customer.Name} - Email:{customer.Email}");
+    }
+    
+    private void SearchCustomerByName()
+    {
+        Console.WriteLine("Enter your customer name:");
+        var name = Console.ReadLine();
+        
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            Console.WriteLine("Please enter a valid name.");
+            return;
+        }
+        
+        var customers = _customerService.SearchCustomerByName(name);
+
+        if (customers.Count == 0)
+        {
+            Console.WriteLine("No customer found.");
+            return;
+        }
+        
+        foreach (var customer in customers)
+        {
+            Console.WriteLine($"Customer Id:{customer.CustomerId} - Name:{customer.Name} - Email:{customer.Email}");
+        }   
+    }
+    
+    private void DeleteCustomer()
+    {
+        Console.WriteLine("Enter your customer ID:");
+        int.TryParse(Console.ReadLine(), out int customerId);
+        
+        if (customerId <= 0)
+        {
+            Console.WriteLine("Please enter a valid customer ID.");
+            return;
+        }
+        
+        var result = _customerService.DeleteCustomer(customerId);
+
+        if (!result)
+        {
+            Console.WriteLine("Delete not successful.");
+            return;
+        }  
+        
+        Console.WriteLine("Customer delete successfully.");
+    }
+
+    private void AccountManagement()
+    {
+        while (true)
+        {
+            Console.WriteLine("===== Account Management =====");
+            Console.WriteLine("1 - Add Account");
+            Console.WriteLine("2 - Show Account");
+            Console.WriteLine("3 - Search Account");
+            Console.WriteLine("4 - Delete Account");
+            Console.WriteLine("5 - Deposit");
+            Console.WriteLine("6 - Withdraw");
+            Console.WriteLine("7 - Transfer");
+            Console.WriteLine("0 - Back");
+
+            if (!int.TryParse(Console.ReadLine(), out int input))
+            {
+                Console.WriteLine("Please enter a valid option.");
+                continue;
+            }
+
+            switch (input)
+            {
+                case 0:
+                    return;
+                case 1:
+                    AddAccount();
+                    break;
+                case 2:
+                    ShowAccounts();
+                    break;
+                case 3:
+                    SearchAccount();
+                    break;
+                case 4:
+                    DeleteAccount();
+                    break;
+                case 5:
+                    Deposit();
+                    break;
+                case 6:
+                    Withdraw();
+                    break;
+                case 7:
+                    Transfer();
+                    break;
+                default:
+                    Console.WriteLine("Please enter a valid option.");
+                    break;
+            }
+        }
+    }
+    
+    private void AddAccount()
+    {
+        Console.WriteLine("Enter your customer ID:");
+        int.TryParse(Console.ReadLine(), out int customerId);
+        
+        if (customerId <= 0)
+        {
+            Console.WriteLine("Please enter a valid customer ID.");
+            return;
+        }
+        Console.WriteLine("Enter your Account type:");
+        Console.WriteLine("0 - Checking");
+        Console.WriteLine("1 - Savings");
+        int.TryParse(Console.ReadLine(), out int accountTypeInput);
+        
+        if (accountTypeInput != 0 && accountTypeInput != 1)
+        {
+            Console.WriteLine("Please enter a valid type.");
+            return;
+        }
+
+        AccountType accountType = (AccountType)accountTypeInput;
+        var result = _accountService.AddAccount(customerId, accountType);
+
+        if (!result)
+        {
+            Console.WriteLine("Account not add successfully."); 
+            return;
+        }
+        
+        Console.WriteLine("Account add successfully.");
+    }
+    
+    private void ShowAccounts()
+    {
+        Console.WriteLine("Account list:");
+        var accounts = _accountService.GetAccounts();
+
+        if (accounts.Count == 0)
+        {
+            Console.WriteLine("No account found.");
+            return;
+        }
+
+        foreach (var account in accounts)
+        {
+            Console.WriteLine($"Account Id:{account.AccountId} - Customer Id:{account.CustomerId} - Account Type:{account.AccountType} - Balance:{account.Balance}");
+        }
+    }
+    
+    private void SearchAccount()
+    {
+        Console.WriteLine("Enter your account ID:");
+        int.TryParse(Console.ReadLine(), out int accountId);
+        
+        if (accountId <= 0)
+        {
+            Console.WriteLine("Please enter a valid account ID.");
+            return;
+        }
+        
+        var account = _accountService.GetAccountById(accountId);
+
+        if (account == null)
+        {
+            Console.WriteLine("No account found.");
+            return;
+        }
+        
+        Console.WriteLine($"Account Id:{account.AccountId} - Customer Id:{account.CustomerId} - Account Type:{account.AccountType} - Balance:{account.Balance}");
+    }
+    
+    private void DeleteAccount()
+    {
+        Console.WriteLine("Enter your account ID:");
+        int.TryParse(Console.ReadLine(), out int accountId);
+        
+        if (accountId <= 0)
+        {
+            Console.WriteLine("Please enter a valid account ID.");
+            return;
+        }
+        
+        var result = _accountService.DeleteAccount(accountId);
+
+        if (!result)
+        {
+            Console.WriteLine("Delete not successful.");
+            return;
+        }
+        
+        Console.WriteLine("Account deleted successfully.");        
+    }
+
+    private void Deposit()
+    {
+        Console.WriteLine("Enter your account ID:");
+        int.TryParse(Console.ReadLine(), out int accountId);
+        
+        if (accountId <= 0)
+        {
+            Console.WriteLine("Please enter a valid account ID.");
+            return;
+        }
+        
+        Console.WriteLine("Enter your amount:");
+        decimal.TryParse(Console.ReadLine(), out decimal amount);
+        
+        if (amount <= 0)
+        {
+            Console.WriteLine("Please enter a valid amount.");
+            return;
+        }
+        
+        var result = _accountService.Deposit(accountId, amount);
+
+        if (!result)
+        {
+            Console.WriteLine("Deposit not successful.");
+            return;
+        }
+        
+        Console.WriteLine("Deposit successful.");       
+    }
+
+    private void Withdraw()
+    {
+        Console.WriteLine("Enter your account ID:");
+        int.TryParse(Console.ReadLine(), out int accountId);
+        
+        if (accountId <= 0)
+        {
+            Console.WriteLine("Please enter a valid account ID.");
+            return;
+        }
+        
+        Console.WriteLine("Enter your withdraw amount:");
+        decimal.TryParse(Console.ReadLine(), out decimal amount);
+        
+        if (amount <= 0)
+        {
+            Console.WriteLine("Please enter a valid withdraw amount.");
+            return;
+        }
+        
+        var result = _accountService.Withdraw(accountId, amount);
+
+        if (!result)
+        {
+            Console.WriteLine("Withdraw not successful.");
+            return;
+        }
+        
+        Console.WriteLine("Withdraw successful.");     
+    }
+
+    private void Transfer()
+    {
+        Console.WriteLine("Enter your from account ID:");
+        int.TryParse(Console.ReadLine(), out int fromAccountId);
+        
+        if (fromAccountId <= 0)
+        {
+            Console.WriteLine("Please enter a valid account ID.");
+            return;
+        }
+        
+        Console.WriteLine("Enter your to account ID:");
+        int.TryParse(Console.ReadLine(), out int toAccountId);
+        
+        if (toAccountId <= 0)
+        {
+            Console.WriteLine("Please enter a valid account ID.");
+            return;
+        }
+        
+        Console.WriteLine("Enter your withdraw amount:");
+        decimal.TryParse(Console.ReadLine(), out decimal amount);
+        
+        if (amount <= 0)
+        {
+            Console.WriteLine("Please enter a valid withdraw amount.");
+            return;
+        }
+        
+        var result = _accountService.Transfer(fromAccountId, toAccountId, amount);
+
+        if (!result)
+        {
+            Console.WriteLine("Transfer not successful.");
+            return;
+        }
+        
+        Console.WriteLine("Transfer successful.");     
+    }
+
+    private void TransactionManagement()
+    {
+        while (true)
+        {
+            Console.WriteLine("===== Transaction =====");
+            Console.WriteLine("1 - Show Transactions");
+            Console.WriteLine("2 - Search Transaction By Id");
+            Console.WriteLine("0 - Back");
+
+            if (!int.TryParse(Console.ReadLine(), out int input))
+            {
+                Console.WriteLine("Please enter a valid option.");
+                continue;
+            }
+
+            switch (input)
+            {
+                case 0:
+                    return;
+                case 1:
+                    ShowTransaction();
+                    break;
+                case 2:
+                    SearchTransaction();
+                    break;
+                default:
+                    Console.WriteLine("Please enter a valid option.");
+                    break;
+            }
+        }
+    }
+    
+    private void ShowTransaction()
+    {
+        Console.WriteLine("Transaction list:");
+        var transactions = _transactionService.GetTransactions();
+
+        if (transactions.Count == 0)
+        {
+            Console.WriteLine("No transaction found.");
+            return;
+        }
+
+        foreach (var transaction in transactions)
+        {
+            Console.WriteLine($"Transaction Id:{transaction.TransactionId} - Account Id:{transaction.AccountId}" +
+                              $" - Transaction Type:{transaction.TransactionType} - Amount:{transaction.Amount} - Date:{transaction.Date}");
+        }        
+    }
+
+    private void SearchTransaction()
+    {
+        Console.WriteLine("Enter your transaction ID:");
+        int.TryParse(Console.ReadLine(), out int transactionId);
+    
+        if (transactionId <= 0)
+        {
+            Console.WriteLine("Please enter a valid transaction ID.");
+            return;
+        }
+    
+        var transaction = _transactionService.GetTransactionById(transactionId);
+
+        if (transaction == null)
+        {
+            Console.WriteLine("No transaction found.");
+            return;
+        }
+    
+        Console.WriteLine($"Transaction Id:{transaction.TransactionId} - Account Id:{transaction.AccountId}" +
+                          $" - Transaction Type:{transaction.TransactionType} - Amount:{transaction.Amount} - Date:{transaction.Date}");
+    }
+}
